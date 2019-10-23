196836e21633aeeb5993104ec52512f6ca5f916b
System Administrator <root@Alonas-MacBook-Pro.local>
2019-10-22 22:40:54 -0700

    Updates

--- src/components/container.js
+++
@@ -1,8 +0,0 @@
-/** @jsx jsx */
-import { jsx } from 'theme-ui'
-
-function Container(props) {
-  return <div sx={{ maxWidth: 'maxs', mx: 'auto', px: 5, py: 8, backgroundColor: '#000000' }} {...props} />
-}
-
-export default Container
